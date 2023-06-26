b7643e963cd5eab6cad5c815dbf8a03341aac244
SantiagoPrice <132046701+SantiagoPrice@users.noreply.github.com>
2023-06-26 12:34:05 +0900

    Add files via upload
    first file upload

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Library for processsing mocap data
+
+Tutorial for creating python library; https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 24 11:49:56 2023
+
+@author: UTAIL
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name='EMGMOCAPproc',    packages=find_packages(include=['EMGMOCAPproc']), 
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',\
+    license='MIT',
+    install_requires = ['c3d','pickleshare','matplotlib'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='Tests',
+)
